ff48a0157ad341d87e754fb76c46a3afbabbab57
ale-seme <alexei.semeraro@gmail.com>
2024-12-22 16:15:34 +0100

    empty ex03

---
+++ ex03/include/Fixed.hpp
@@ -0,0 +1,68 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <ostream>
+
+
+/*I need to create a class in Orthodox Canonical Form that represents a fixed-point-number
+
+Default constructor
+Copy constructor
+Copy assignment operator
+Destructor
+
+*/
+
+class Fixed{
+
+	private:
+	int					_fixedPoint;
+	static const int	_fractionalBits;
+
+	public:
+
+	Fixed();//default constructor
+	Fixed(const int intValue);
+	Fixed(const float floatValue);
+
+	Fixed(const Fixed &other);//copy constructor
+	Fixed &operator=(const Fixed &other);//operator = overload
+
+	Fixed &operator++();
+	Fixed operator++(int);
+	Fixed &operator--();
+	Fixed operator--(int);
+
+	Fixed operator+(const Fixed &other) const;
+	Fixed operator-(const Fixed &other) const ;
+	Fixed operator*(const Fixed &other) const ;
+	Fixed operator/(const Fixed &other) const ;
+	
+	bool operator>(const Fixed &other) const;
+	bool operator<(const Fixed &other) const;
+	bool operator>=(const Fixed &other) const;
+	bool operator<=(const Fixed &other) const ;
+	bool operator!=(const Fixed &other) const;
+	bool operator==(const Fixed &other) const;
+
+
+	~Fixed();//Destructor
+
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+	static Fixed &min(Fixed &f1, Fixed &f2);
+	static const Fixed &min(const Fixed &f1, const Fixed &f2);
+
+	static Fixed &max(Fixed &f1, Fixed &f2);
+	static const Fixed&max(const Fixed &f1, const Fixed &f2); 
+
+	
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &f);
+
+
+#endif
---
+++ ex03/src/main.cpp
@@ -0,0 +1,90 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+// Fixed a;
+// Fixed const b( 10 );
+// Fixed const c( 42.42f );
+// Fixed const d( b );
+// a = Fixed( 1234.4321f );
+// std::cout << "a is " << a << std::endl;
+// std::cout << "b is " << b << std::endl;
+// std::cout << "c is " << c << std::endl;
+// std::cout << "d is " << d << std::endl;
+// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+
+int main( void ) {
+
+try {
+Fixed a;
+
+
+Fixed b(Fixed( 5.2f ) * Fixed(2));
+
+std::cout << "value of b: " << b << std::endl;
+
+Fixed c = Fixed (20.4f) / Fixed(4);
+
+std::cout << "value of c " << c << std::endl;
+
+Fixed d(c);
+
+std::cout << "value of d: " << d << " is the same of c: " << c << std::endl;
+
+
+Fixed e = c + d;
+Fixed f = e - d;
+
+std::cout << "value of f: " << f << " is the same of d: " << c << std::endl;
+
+if (b >= c * 2)
+    std::cout << b << " >= " << c * 2 << std::endl;
+if (b > c/2)
+    std::cout << b << " > " << c / 2 << std::endl;
+if (c <= b)
+    std::cout << b << " <= " << c << std::endl;
+if (b < c)
+    std::cout << b << " < " << c << std::endl;
+if (b == c)
+    std::cout << b << " == " << c << std::endl;
+else if (b != c)
+    std::cout << b << " != " << c << std::endl;
+
+
+std::cout << std::endl << "NOW ORIGINAL TEST FROM THE SUBJECT:" << std::endl << std::endl;
+
+
+Fixed g;
+Fixed const h(Fixed( 5.05f ) * Fixed (2));
+Fixed const i( 42.42f );
+Fixed const l( h );
+
+std::cout << g << std::endl;
+std::cout << ++g << std::endl;
+std::cout << g << std::endl;
+std::cout << g++ << std::endl;
+std::cout << g << std::endl;
+
+std::cout << h << std::endl;
+
+std::cout << "The minimum between " << g << " and " << h << " is: " << Fixed::min(g, h) << std::endl;
+std::cout << "The maximum between " << g << " and " << h << " is: " << Fixed::max(g, h) << std::endl;
+
+Fixed test1(5.01f);
+
+Fixed test2(5.009f);
+
+if (test1 > test2)
+    std::cout << test1 << " > " << test2 << std::endl;
+
+}
+catch (const std::exception &e){
+    std::cerr << "Exception caught: " << e.what() << std::endl;
+    return 1;
+}
+return (0);
+
+}
